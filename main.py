ec5c0b0855d7bf922066f082f90cccab8424ba03
yusuf <yus>
2025-01-01 23:18:58 -0500

    integrated multi headed attention, added feedforward, and block

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import torch.nn.functional as F
 learning_rate = 1e-3
 batch_size = 64
 block_size = 256
-embed_size = 32
+embed_size = 384
 device = "cuda"
-max_iters = 10000
-
+max_iters = 5000
+n_heads,n_layers = 6,6
 
 #shakespeare text
 with open("./input.txt","r") as f:
@@ -44,20 +44,20 @@ def get_batch(mode:str):
 
 
 
+
 class Head(nn.Module):
-    def __init__(self):
+    def __init__(self,head_size):
         super().__init__()
         #Q,K,V
-        self.query = nn.Linear(embed_size,embed_size,bias=False)
-        self.key = nn.Linear(embed_size,embed_size,bias=False)
-        self.value = nn.Linear(embed_size,embed_size,bias=False)
+        self.query = nn.Linear(embed_size,head_size,bias=False)
+        self.key = nn.Linear(embed_size,head_size,bias=False)
+        self.value = nn.Linear(embed_size,head_size,bias=False)
     def forward(self,x):
-        #x is B,T,EMB
+        #x is B,T,HZ
         q = self.query(x)
         k = self.key(x)
         v = self.value(x)
-
-        #B,T,EMB @ B,EMB,T => B,T,T
+        #B,T,HZ @ B,HZ,T => B,T,T
         tmp = (q @ k.transpose(-2,-1)) * ((k.shape[-1])**-0.5)
         #remove token's awareness of future
         tmp = torch.tril(tmp)
@@ -69,23 +69,72 @@ class Head(nn.Module):
         return out
 
 
+class MultiAttentionHead(nn.Module):
+    def __init__(self,head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(n_heads)])
+        self.lin = nn.Linear(head_size*n_heads,embed_size)
+    def forward(self,x):
+        #since each head will have B,T,HEAD_SIZE we concatenate on the last dimensions to get back to embed size.since HEAD_SIZE * n_heads == embed_size
+        out = torch.cat([h(x) for h in self.heads],dim=-1)
+        out = self.lin(out)
+        return out
+    
+class FeedForward(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.nn = nn.Sequential(
+            nn.Linear(embed_size,embed_size),
+            nn.ReLU(),
+            nn.Linear(embed_size,embed_size)
+        )
+    def forward(self,x):
+        x = self.nn(x)
+        return x
+    
+
+class Block(nn.Module):
+    def __init__(self):
+        super().__init__()
+        head_size = embed_size // n_heads
+        self.sa_heads = MultiAttentionHead(head_size)
+        self.ffwd = FeedForward()
+        self.ln1 = nn.LayerNorm(embed_size)
+        self.ln2 = nn.LayerNorm(embed_size)
+    def forward(self,x):
+        x = self.ln1(x + self.sa_heads(x))
+        x = self.ln2(x + self.ffwd(x))
+        return x 
+
+
+
+
+
 
 class GPTModel(nn.Module):
 
     def __init__(self):
         super().__init__()
+        #vec and positinal embeddings
         self.token_embedding = nn.Embedding(vocab_size, embed_size)
         self.pos_embedding = nn.Embedding(block_size,embed_size)
-        self.final_linear = nn.Linear(embed_size,vocab_size)
-        self.head = Head().to(device)
+        #final layer norm
         self.ln_f = nn.LayerNorm(embed_size)
+        #multiple iterations of blocks
+        self.nn_blocks = nn.Sequential(*[Block() for _ in range(n_layers)])
+        #final linear transformation
+        self.lm_head = nn.Linear(embed_size,vocab_size)
     def forward(self,x,targets=None):
         B,T = x.shape
         x = self.token_embedding(x)
+        #add token embed + pos embed
         x = x + self.pos_embedding(torch.arange(T,device=device))
-        x = self.head(x)
+        #self attention
+        x = self.nn_blocks(x)
+        #normalize
         x = self.ln_f(x)
-        logits = self.final_linear(x)
+        #linear from embed to vocab size
+        logits = self.lm_head(x)
 
         if targets is None:
             loss = None
@@ -127,7 +176,7 @@ def generate(x, max_new_tokens):
 
 
 train()
-#torch.save(model.state_dict(),"./model.pth")
+torch.save(model.state_dict(),"./model.pth")
 #model.load_state_dict(torch.load("./model.pth",weights_only=True))
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 answer = decode(generate(context, max_new_tokens=500)[0].tolist())
